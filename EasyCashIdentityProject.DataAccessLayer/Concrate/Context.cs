3e4c7978b5f48b881e43164574b02dcc9a00713d
ömer dikişli <omerdksli@gmail.com>
2024-02-28 01:36:24 +0300

    created database with migrate method

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrate/Context.cs
@@ -0,0 +1,24 @@
+﻿using EasyCashIdentityProject.EntityLayer;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrate
+{
+    public class Context:IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-623S80S\\SQLEXPRESS;initial catalog=EasyCashDb;integrated Security=true");
+
+        }
+        public DbSet<CostumerAccount> CostumerAccounts { get; set; }
+        public DbSet<CostumerAccountProcess> CostumerAccountProcesses { get; set; }
+
+
+    }
+}
